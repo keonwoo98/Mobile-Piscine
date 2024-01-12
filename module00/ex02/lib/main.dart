be3965f49bcfaae39e38d21633bd51cab6bee136
keonwoo98 <keonwoo98@naver.com>
2024-01-12 20:10:22 +0100

    made calculator display ex02

--- module00/ex02/lib/main.dart
+++ module00/ex02/lib/main.dart
@@ -7,119 +7,159 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'mobileModule00/ex02',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Calculator(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Calculator> createState() => _CalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _CalculatorState extends State<Calculator> {
+  String expression = '0';
+  String result = '0';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text(
+          'Calculator',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Text(
+                  //   expression,
+                  //   textAlign: TextAlign.right,
+                  //   style: const TextStyle(
+                  //     fontSize: 40.0,
+                  //   ),
+                  // ),
+                  // Text(
+                  //   result,
+                  //   textAlign: TextAlign.right,
+                  //   style: const TextStyle(
+                  //     fontSize: 40.0,
+                  //   ),
+                  // ),
+                  TextField(
+                    controller: TextEditingController(text: expression),
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(fontSize: 32),
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none, // 밑줄을 없애는 부분
+                      // contentPadding: EdgeInsets.zero, // 내용과 상하좌우 간격을 없애는 부분
+                    ),
+                  ),
+                  TextField(
+                    controller: TextEditingController(text: result),
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(fontSize: 32),
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none, // 밑줄을 없애는 부분
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                double maxButtonWidth = constraints.maxWidth / 4;
+                double maxButtonHeight = constraints.maxHeight / 4;
+
+                return GridView.count(
+                  crossAxisCount: 5,
+                  padding: const EdgeInsets.all(4.0),
+                  mainAxisSpacing: 4.0,
+                  crossAxisSpacing: 4.0,
+                  childAspectRatio: maxButtonWidth / maxButtonHeight,
+                  children: <String>[
+                    '7',
+                    '8',
+                    '9',
+                    'C',
+                    'AC',
+                    '4',
+                    '5',
+                    '6',
+                    '+',
+                    '-',
+                    '1',
+                    '2',
+                    '3',
+                    'x',
+                    '/',
+                    '0',
+                    '.',
+                    '00',
+                    '=',
+                    '',
+                  ].map((key) {
+                    return GridTile(
+                      child: KeyboardKey(key),
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class KeyboardKey extends StatelessWidget {
+  const KeyboardKey(this._keyValue, {Key? key}) : super(key: key);
+
+  final _keyValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero, // 여기서 모서리를 제거합니다.
+        ),
+      ),
+      onPressed: () {},
+      child: Text(
+        _keyValue,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 22.0,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
