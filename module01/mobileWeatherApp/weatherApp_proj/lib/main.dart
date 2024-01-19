cd4a75c42507de0fe8f3ee01b4d6721c3017dd3c
keonwoo98 <keonwoo98@naver.com>
2024-01-19 16:02:27 +0100

    finished weatherApp_proj

--- module01/mobileWeatherApp/weatherApp_proj/lib/main.dart
+++ module01/mobileWeatherApp/weatherApp_proj/lib/main.dart
@@ -1,125 +1,121 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyWeatherApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyWeatherApp extends StatelessWidget {
+  const MyWeatherApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'mobileModule01/weatherApp_proj',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WeatherHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WeatherHomePage extends StatefulWidget {
+  const WeatherHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WeatherHomePage> createState() => _WeatherHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WeatherHomePageState extends State<WeatherHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+  late TextEditingController searchController;
+  int selectedIndex = 0;
+  String searchText = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            controller: searchController,
+            onSubmitted: (text) => setState(() {
+              searchText = text;
+            }),
+            decoration: const InputDecoration(
+              hintText: 'Search',
+              icon: Icon(Icons.search),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () => setState(() {
+                if (searchText.contains('Geolocation')) {
+                  searchText = '';
+                } else {
+                  searchController.text = '';
+                  searchText = 'Geolocation';
+                }
+              }),
+              icon: !searchText.contains('Geolocation')
+                  ? const Icon(Icons.location_off_outlined)
+                  : const Icon(Icons.location_on_sharp),
             ),
           ],
         ),
+        body: TabBarView(
+          physics: const BouncingScrollPhysics(),
+          controller: tabController,
+          children: [
+            TabContent(title: 'Currently Tab Content\n$searchText'),
+            TabContent(title: 'Today Tab Content\n$searchText'),
+            TabContent(title: 'Weekly Tab Content\n$searchText'),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          clipBehavior: Clip.none,
+          child: TabBar(
+            controller: tabController,
+            indicator: null,
+            tabs: const [
+              Tab(icon: Icon(Icons.wb_sunny_outlined), text: 'Currently'),
+              Tab(icon: Icon(Icons.today_outlined), text: 'Today'),
+              Tab(icon: Icon(Icons.calendar_today_outlined), text: 'Weekly'),
+            ],
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TabContent extends StatelessWidget {
+  final String title;
+  const TabContent({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(title),
     );
   }
 }
