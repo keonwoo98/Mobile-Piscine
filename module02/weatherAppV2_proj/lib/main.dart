dbf2a80970b57f2d9bb72d1b16d2f613ad116d17
keonwoo98 <keonwoo98@naver.com>
2024-01-23 14:29:41 +0100

    finished ex00

--- module02/weatherAppV2_proj/lib/main.dart
+++ module02/weatherAppV2_proj/lib/main.dart
@@ -1,125 +1,200 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'mobileModule02/weatherAppV2_proj',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const WeatherHomePage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WeatherHomePage extends StatelessWidget {
+  const WeatherHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+    var appState = context.watch<MyAppState>();
+
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: MyAppBar(theme: Theme.of(context), appState: appState),
+        body: const TabBarView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            TabContent(title: 'Currently'),
+            TabContent(title: 'Today'),
+            TabContent(title: 'Weekly'),
           ],
         ),
+        bottomNavigationBar: const MyBottomAppBar(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final ThemeData theme;
+  final MyAppState appState;
+
+  const MyAppBar({super.key, required this.theme, required this.appState});
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController textFieldController = TextEditingController();
+
+    return AppBar(
+      backgroundColor: theme.colorScheme.primary,
+      title: TextField(
+        controller: textFieldController,
+        cursorColor: theme.colorScheme.onPrimary,
+        style: TextStyle(color: theme.colorScheme.onPrimary),
+        onSubmitted: (text) => appState.setLocation(textFieldController.text),
+        decoration: InputDecoration(
+          hintText: 'Search',
+          hintStyle: TextStyle(color: theme.colorScheme.onPrimary),
+          prefixIcon: GestureDetector(
+            onTap: () => appState.setLocation(textFieldController.text),
+            child: Icon(
+              Icons.search,
+              color: theme.colorScheme.onPrimary,
+            ),
+          ),
+          suffixIcon: GestureDetector(
+            onTap: () async => appState.getGeolocation(),
+            child: Icon(
+              Icons.gps_fixed_rounded,
+              color: theme.colorScheme.onPrimary,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => AppBar().preferredSize;
+}
+
+class MyBottomAppBar extends StatelessWidget {
+  const MyBottomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BottomAppBar(
+      clipBehavior: Clip.none,
+      child: TabBar(
+        indicator: null,
+        tabs: [
+          Tab(icon: Icon(Icons.wb_sunny_outlined), text: 'Currently'),
+          Tab(icon: Icon(Icons.today_outlined), text: 'Today'),
+          Tab(icon: Icon(Icons.calendar_today_outlined), text: 'Weekly'),
+        ],
+      ),
+    );
+  }
+}
+
+class TabContent extends StatelessWidget {
+  final String title;
+  const TabContent({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    final theme = Theme.of(context);
+    final locationStyle = theme.textTheme.headlineMedium!.copyWith(
+      color: theme.colorScheme.primary,
+      fontWeight: FontWeight.bold,
+    );
+    final titleStyle =
+        theme.textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold);
+    final errorStyle = titleStyle.copyWith(color: theme.colorScheme.error);
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: !appState.geolocationPermError
+          ? [
+              Text(
+                title,
+                style: titleStyle,
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                appState.location,
+                style: locationStyle,
+                textAlign: TextAlign.center,
+              ),
+            ]
+          : [
+              Text(
+                'Geolocation is not available, please enable it in you App settings',
+                style: errorStyle,
+                textAlign: TextAlign.center,
+              )
+            ],
+    ));
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  String location = '';
+  bool geolocationPermError = false;
+
+  void setLocation(String input) {
+    location = input;
+    geolocationPermError = false;
+    notifyListeners();
+  }
+
+  void setGeolocationPermError(bool value) {
+    geolocationPermError = value;
+    notifyListeners();
+  }
+
+  Future<int> getGeoPermission() async {
+    bool geoEnabled = false;
+    geoEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!geoEnabled) {
+      setGeolocationPermError(true);
+      return -1;
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setGeolocationPermError(true);
+        return -1;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      setGeolocationPermError(true);
+      return -1;
+    }
+    return 0;
+  }
+
+  void getGeolocation() async {
+    if (await getGeoPermission() == -1) return;
+    Position geolocation = await Geolocator.getCurrentPosition();
+    setLocation(geolocation.toString());
   }
 }
