84cd3f75fe2fa19ee0d01912407621c0d15922cd
keonwoo98 <keonwoo98@naver.com>
2024-02-27 13:00:16 +0100

    connect firebase

--- module04/diaryapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ module04/diaryapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import auth0_flutter
+import firebase_core
+import firebase_database
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  Auth0FlutterPlugin.register(with: registry.registrar(forPlugin: "Auth0FlutterPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
 }
