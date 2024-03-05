b7f437bed951dbee475c7efd6204e587be3839a1
keonwoo98 <keonwoo98@naver.com>
2024-03-05 20:40:22 +0100

    add method getting icon name

--- module04/diaryapp/lib/models/feeling.dart
+++ module04/diaryapp/lib/models/feeling.dart
@@ -6,15 +6,15 @@ class Feeling {
 
   IconData getIcon() {
     switch (iconName) {
-      case "dissatisfied":
+      case 'dissatisfied':
         return Icons.sentiment_dissatisfied_outlined;
-      case "very_dissatisfied":
+      case 'very_dissatisfied':
         return Icons.sentiment_very_dissatisfied_outlined;
-      case "neutral":
+      case 'neutral':
         return Icons.sentiment_neutral_outlined;
-      case "satisfied":
+      case 'satisfied':
         return Icons.sentiment_satisfied_alt_outlined;
-      case "very_satisfied":
+      case 'very_satisfied':
         return Icons.sentiment_very_satisfied_outlined;
       default:
         return Icons.sentiment_neutral_outlined;
@@ -23,18 +23,35 @@ class Feeling {
 
   Color getColor() {
     switch (iconName) {
-      case "dissatisfied":
+      case 'dissatisfied':
         return Colors.orange;
-      case "very_dissatisfied":
+      case 'very_dissatisfied':
         return Colors.red;
-      case "neutral":
+      case 'neutral':
         return Colors.purple;
-      case "satisfied":
+      case 'satisfied':
         return Colors.green;
-      case "very_satisfied":
+      case 'very_satisfied':
         return Colors.blue;
       default:
         return Colors.yellow;
+    }
+  }
+
+  static Feeling fromIconName(String iconName) {
+    switch (iconName) {
+      case 'dissatisfied':
+        return Feeling(iconName: iconName); // 여기에 필요한 아이콘 등을 추가
+      case 'very_dissatisfied':
+        return Feeling(iconName: iconName);
+      case 'neutral':
+        return Feeling(iconName: iconName);
+      case 'satisfied':
+        return Feeling(iconName: iconName);
+      case 'very_satisfied':
+        return Feeling(iconName: iconName);
+      default:
+        return Feeling(iconName: 'neutral'); // 기본값 설정
     }
   }
 }
