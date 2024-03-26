590ccf175e485a138aed129f2d25c3ac53f0d6b8
keonwoo98 <keonwoo98@naver.com>
2024-03-26 19:45:01 +0100

    Wrap StreamBuilder with Card

--- module04/diaryapp/lib/widgets/entries_list.dart
+++ module04/diaryapp/lib/widgets/entries_list.dart
@@ -22,49 +22,57 @@ class EntriesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: entryStream,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return const Center(child: Text('No entries found'));
-        }
-        return ListView.builder(
-          shrinkWrap: true,
-          physics: isScrollable
-              ? const AlwaysScrollableScrollPhysics()
-              : const NeverScrollableScrollPhysics(),
-          itemCount: snapshot.data!.docs.length,
-          itemBuilder: (context, index) {
-            var doc = snapshot.data!.docs[index].data() as Map<String, dynamic>;
-            var entryModel = Entry.fromSnapshot(snapshot.data!.docs[index]);
-            String formattedDate = formatTimestamp(doc['date']);
-            IconData icon = Feeling.getIcon(doc['icon']);
-            Color iconColor = Feeling.getColor(doc['icon']);
+    return Card(
+      margin: const EdgeInsets.all(16.0),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: entryStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return const Center(child: Text('No entries found'));
+            }
+            return ListView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(bottom: 10.0),
+              physics: isScrollable
+                  ? const AlwaysScrollableScrollPhysics()
+                  : const NeverScrollableScrollPhysics(),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var doc =
+                    snapshot.data!.docs[index].data() as Map<String, dynamic>;
+                var entryModel = Entry.fromSnapshot(snapshot.data!.docs[index]);
+                String formattedDate = formatTimestamp(doc['date']);
+                IconData icon = Feeling.getIcon(doc['icon']);
+                Color iconColor = Feeling.getColor(doc['icon']);
 
-            return Container(
-              margin: const EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: ListTile(
-                title: Text(doc['title'],
-                    style: const TextStyle(fontWeight: FontWeight.bold)),
-                subtitle: Text(formattedDate),
-                trailing: Icon(icon, color: iconColor),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) =>
-                          EntryDetailPage(entry: entryModel)));
-                },
-              ),
+                return Container(
+                  margin: const EdgeInsets.fromLTRB(10.0, 12.0, 10.0, 0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: ListTile(
+                    title: Text(doc['title'],
+                        style: const TextStyle(fontWeight: FontWeight.bold)),
+                    subtitle: Text(formattedDate),
+                    trailing: Icon(icon, color: iconColor),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              EntryDetailPage(entry: entryModel)));
+                    },
+                  ),
+                );
+              },
             );
           },
-        );
-      },
+        ),
+      ),
     );
   }
 }
